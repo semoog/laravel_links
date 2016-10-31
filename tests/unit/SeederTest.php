cef84f1efb689d2540c9cf4b5be442df4972c72a
Sebastian Ernst <sebmernst@gmail.com>
2016-10-31 11:30:42 -0600

    factory and unit test pass

--- tests/unit/SeederTest.php
+++ tests/unit/SeederTest.php
@@ -1,3 +1,14 @@
-public function testLinksTable(){
- $this->seeInDatabase('links', ['title' => 'dotdev.co']);
+<?php
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+class SeederTest extends TestCase
+{
+    public function testLinksTable()
+    {
+        factory(App\Link::class)->create([
+            'title' => 'dotdev.co',
+        ]);
+        $this->seeInDatabase('links', ['title' => 'dotdev.co']);
+    }
 }
